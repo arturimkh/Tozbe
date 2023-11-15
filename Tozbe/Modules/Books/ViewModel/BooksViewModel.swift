d24ae59caec3d327c9bff6c2cad2773b5fdeefdd
Artur <imanbaevart123@gmail.com>
2023-11-15 20:51:19 +0300

    Added UserManager

---
+++ Tozbe/Modules/Books/ViewModel/BooksViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  BooksViewModel.swift
+//  Tozbe
+//
+//  Created by Artur Imanbaev on 15.11.2023.
+//
+
+import Foundation
+class BooksViewModel {
+    private let userManager: UserManager?
+    init(userManager: UserManager){
+        self.userManager = userManager
+    }
+    public func didUserLoggedIn() -> Bool {
+        let state = UserManager.isUserLoggedIn()
+        return state
+    }
+}
